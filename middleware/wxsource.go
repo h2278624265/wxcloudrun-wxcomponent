6804366877d6a0f50ffb4b6dae43cac26f65b2ea
Hengzhi Wu <cshzwoo@163.com>
2022-11-19 18:26:12 +0800

    update

--- middleware/wxsource.go
+++ middleware/wxsource.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"encoding/xml"
 
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/errno"
 	"github.com/WeixinCloud/wxcloudrun-wxcomponent/comm/utils"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
+	// "github.com/gin-gonic/gin/binding"
 )
+
+type xmlCallbackComponentRecord struct {
+	AppId string `xml:"AppId"`
+	Encrypt string `xml:"Encrypt"`
+}
 
 // WXSourceMiddleWare 中间件 判断是否来源于微信
 func WXSourceMiddleWare(c *gin.Context) {
@@ -26,14 +32,17 @@ func WXSourceMiddleWare(c *gin.Context) {
 func DecryptContext(c *gin.Context) {
 	body, _ := ioutil.ReadAll(c.Request.Body)
 	fmt.Println("body: ", body)
-	
-	var xmlBody string
-	if err := binding.XML.BindBody(body, &xmlBody); err != nil {
+
+	xmlBody := xmlCallbackComponentRecord{}
+	err := xml.Unmarshal(body, &xmlBody)
+
+	if err != nil {
 		c.JSON(http.StatusOK, errno.ErrInvalidParam.WithData(err.Error()))
 		return
 	}
+	// fmt.Println("XML body: ", xmlBody)
 	fmt.Println("XML body: ", xmlBody)
-	utils.DecryptReqContext(xmlBody)
+	utils.DecryptReqContext(xmlBody.Encrypt)
 	// if errXml := c.ShouldBindBodyWith(&xmlBody, binding.XML); errXml == nil {
 	// 	fmt.Println("XML body: ", xmlBody)
 	// 	utils.DecryptReqContext(xmlBody)
